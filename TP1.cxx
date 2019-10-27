0d5b8008cf0170b56c46ce1c806a98615130edbd
Ianel <48760301+Ianel@users.noreply.github.com>
2019-10-27 19:26:02 +0300

    Ajout du choix de niveau
    Probleme d'arret du jeu

--- TP1.cxx
+++ TP1.cxx
@@ -8,17 +8,42 @@ int main()
 {
 	/*Initialisation des variables et des constantes*/
 	bool continuerPartie = 1;
-	const int MAX = 100, MIN = 1;
+    int nombreMax, niveau;
 	int nombreMystere, nombreEntre, cpt = 0;
+	const int MIN = 1;
 	char choix;
 
 	do { //Debut de la 1ere boucle
+    	do {
+    	printf("Choisissez le niveau de difficultés: \n");
+    	printf("1- De 1 à 10: tapez 1 \n");
+    	printf("2- De 1 à 100: tapez 2 \n");
+    	printf("3- De 1 à 1000: tapez 3 \n");
+    	printf("---------------------------------------\n");
+    	scanf("%d", &niveau);
+	
+	switch(niveau) {
+	    case 1:
+	        nombreMax = 10;
+	        break;
+	    case 2:
+	        nombreMax = 100;
+	        break;
+	    case 3:
+	        nombreMax = 1000;
+	        break;
+	     default:
+	         printf("Entrer une valeur valide svp!");
+	         break;
+	}
+	     }  while (niveau < 0 && niveau > 4);
+	         
 	srand(time(NULL));
-	nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+	nombreMystere = (rand() % (nombreMax - MIN + 1)) + MIN;
 	
 	do { //Debut de la 2nde boucle
 		cpt++; //Le compteur ne marche pas
-		printf("Entrer un nombre compris entre 1 et 100\n");
+		printf("Entrer un nombre compris entre 1 et %d \n", nombreMax);
 		scanf("%d", &nombreEntre);
 		
 		if (nombreEntre < nombreMystere)
@@ -35,7 +60,7 @@ int main()
 
 	do { //Debut de la derniere boucle
 	printf("Tapez 1 pour continuer et 2 pour quitter\n");
-	scanf("%d", &choix);
+	scanf("%c", &choix);
 	
 	switch (choix) {
 		case 1:
